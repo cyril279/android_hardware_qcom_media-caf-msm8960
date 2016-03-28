3d88d2f5b6fc5f24461786901661134ca1d4fbb6
cyril279 <cyril.x9@gmail.com>
2016-03-28 08:09:09 -0400

    Revert "media: Use direct project pathmap"
    This reverts commit bd070cb5cb289a32faca11f7892c2c45f1855c51.

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
+ifeq ($(call my-dir),$(call project-path-for,qcom-media)/$(TARGET_BOARD_PLATFORM))
 
 ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
 
